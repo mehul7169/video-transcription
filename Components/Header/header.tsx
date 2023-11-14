a3cb5bc6a7a901587697a4f590f5b75040c365c1
Mehul <mehul7169@gmail.com>
2023-11-14 17:33:40 -0500

    first commit

---
+++ Components/Header/header.tsx
@@ -0,0 +1,6 @@
+import React, {FC} from 'react'
+
+const Header: FC = () => {
+}
+
+export default memo(Header)
---
+++ Components/Recorder/recorder.tsx
@@ -0,0 +1,6 @@
+import React, {FC} from 'react'
+
+const Recorder: FC = () => {
+}
+
+export default memo(Recorder)
